f2d0b5963ee6ed93b4580e67cdac18c4924038ed
liangyh <liangyh@spinsoft.com>
2020-09-16 13:38:23 +0800

    新增图片的下载

--- vue.config.js
+++ vue.config.js
@@ -39,6 +39,7 @@ module.exports = {
   productionSourceMap: false,
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
   devServer: {
+    port: 8068,
     proxy: {
       '/tencent': {
         target: 'https://api.weixin.qq.com',
